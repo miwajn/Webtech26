188bdd2aef56a5b3e80990b9772ca8fd4d681a35
unknown <janina_mitwalli@yahoo.de>
2026-06-21 18:57:30 +0200

    Aktualisierung style, text, grid

--- Projekte/semesteraufgabe/src/app/app.ts
+++ Projekte/semesteraufgabe/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './header/header';
 import { Nav } from './nav/nav';
 import { Footer } from './footer/footer';
 
 @Component({
   selector: 'medCycle-root',
-  imports: [RouterOutlet, Header, Nav, Footer],
+  imports: [RouterOutlet, Nav, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
