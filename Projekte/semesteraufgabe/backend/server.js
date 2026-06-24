1bc41d8a212e32bc6231dab66100d56e3fb37a70
unknown <janina_mitwalli@yahoo.de>
2026-06-24 14:31:15 +0200

    MongoDB installation

---
+++ Projekte/semesteraufgabe/backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json()); //Alle JS-Onjekte werden in JSON umgewandelt
+app.use('/', routes);
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`Server started and listening on port ${PORT} ... `);
+    }
+});
