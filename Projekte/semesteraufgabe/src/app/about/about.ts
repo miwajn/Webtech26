7eea64ce12aaf8f0b1a21b482aeec1262dd03b45
unknown <janina_mitwalli@yahoo.de>
2026-07-06 19:06:07 +0200

    Update Home und About us

--- Projekte/semesteraufgabe/src/app/about/about.ts
+++ Projekte/semesteraufgabe/src/app/about/about.ts
@@ -6,4 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
-export class About {}
+export class About {
+
+  openCardIndex: number | null = null;  // null = keine Karte geöffnet
+
+  toggle(event: MouseEvent, index: number): void {
+    const cards = document.querySelectorAll('.kontakt-card');
+    cards.forEach(card => card.classList.remove('open')); // geht alle Cards durch und entfernt Klasse "open"
+
+    const text = document.querySelectorAll('.kontakt-card-text');
+    text.forEach(i => i.classList.add('hidden')); // versteckt die texte
+
+    if (event.currentTarget instanceof HTMLElement) { //TS weiß so, dass card ein HTML-Element ist
+      const card = event.currentTarget;
+
+      if (this.openCardIndex !== index) {
+        card.classList.add('open');
+        card.querySelector('.kontakt-card-text')?.classList.remove('hidden');
+        this.openCardIndex = index;
+      } else {
+        this.openCardIndex = null;
+      }
+    }
+  }
+}
--- Projekte/semesteraufgabe/src/app/home/home.ts
+++ Projekte/semesteraufgabe/src/app/home/home.ts
@@ -8,11 +8,25 @@ import { Component } from '@angular/core';
 })
 export class Home {
 
-  toggle(event: MouseEvent): void {
-    const card = (event.currentTarget as HTMLElement);
-    card.classList.toggle('open');
-    const text = card.querySelector('.vorsorge-card-text');
-    text?.classList.toggle('hidden');
-}
+  openCardIndex: number | null = null;  // null = keine Karte geöffnet
 
+  toggle(event: MouseEvent, index: number): void {
+    const cards = document.querySelectorAll('.vorsorge-card');
+    cards.forEach(card => card.classList.remove('open')); // geht alle Cards durch und entfernt Klasse "open"
+
+    const text = document.querySelectorAll('.vorsorge-card-text');
+    text.forEach(i => i.classList.add('hidden')); // versteckt die texte
+
+    if (event.currentTarget instanceof HTMLElement) { //TS weiß so, dass card ein HTML-Element ist
+      const card = event.currentTarget;
+
+      if (this.openCardIndex !== index) {
+        card.classList.add('open');
+        card.querySelector('.vorsorge-card-text')?.classList.remove('hidden');
+        this.openCardIndex = index;
+      } else {
+        this.openCardIndex = null;
+      }
+    }
+  }
 }
